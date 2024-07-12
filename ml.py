cef203480cfcabd749706a6d463f4f1b1c7319e9
Youssef Roman <youssef.roman@soti.net>
2024-07-12 12:11:08 -0400

    remove ppsqft and Add CSS

--- ml.py
+++ ml.py
@@ -19,22 +19,28 @@ def predict():
     # Get the input features from the form and convert to float
     bedrooms = float(request.form['bedrooms'])
     sqft = float(request.form['sqft'])
-    ppsqft = float(request.form['ppsqft'])
     wr = float(request.form['wr'])
-    year = float(request.form['year'])
     age = float(request.form['age'])
     primerate = float(request.form['primerate'])
     city = float(request.form['city'])
     type_category = float(request.form['type'])
     walkscore = float(request.form['walkscore'])
-
+    
+    # Map walkscore_category to the average value of the range
+    walkscore_map = {
+        0: (90 + 100) / 2,  # Walker’s Paradise
+        1: (70 + 89) / 2,   # Very Walkable
+        2: (50 + 69) / 2,   # Somewhat Walkable
+        3: (25 + 49) / 2,   # Car-Dependent (Most errands require a car)
+        4: (0 + 24) / 2     # Car-Dependent (Almost all errands require a car)
+    }
+    walkscore = walkscore_map[walkscore]
+    
     # Create a DataFrame from the input data
     input_data = pd.DataFrame({
         'Total Bedrooms': [bedrooms],
         'SqFt Numeric': [sqft],
-        'ppsqft': [ppsqft],
         'WR': [wr],
-        'Year': [year],
         'Age Numeric': [age],
         'PrimeRate': [primerate],
         'City Category': [city],
